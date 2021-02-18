0bc582e58b1396dde3afa637e3eaba59ddad1e1b
REDA BENHAMID <redabenhamid@yahoo.com>
2021-02-18 22:05:44 +0100

    Deploy App Laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+﻿@extends('index')
+
+
+@section('main_block')
+
+<!-- Main Content Starts -->
+<section class="container-fluid main-container container-home p-0 revealator-slideup revealator-once revealator-delay1">
+    <div class="color-block d-none d-lg-block"></div>
+    <div class="row home-details-container align-items-center">
+        <div class="col-lg-4 bg position-fixed d-none d-lg-block"></div>
+        <div class="col-12 col-lg-8 offset-lg-4 home-details text-left text-sm-center text-lg-left">
+            <div>
+                <img src="img/img-mobile-light.jpg" class="img-fluid main-img-mobile d-none d-sm-block d-lg-none" alt="my picture" />
+                <h6 class="text-uppercase open-sans-font mb-0 d-block d-sm-none d-lg-block">hi there !</h6>
+                <h1 class="text-uppercase poppins-font"><span>I'm</span> Reda Benhamid</h1>
+                <p class="open-sans-font">I'm a Moroccan based web designer & front‑end developer focused on crafting clean & user‑friendly experiences, I am passionate about building excellent software that improves the lives of those around me.</p>
+                <a href="{{ url('about') }}" class="btn btn-about">more about me</a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Main Content Ends -->
+
+@endsection
